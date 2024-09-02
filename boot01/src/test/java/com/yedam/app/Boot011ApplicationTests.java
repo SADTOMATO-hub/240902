05dfc53586c214b450284ef0ee3a719732db4c96
admin <admin@DESKTOP-1KTB0UQ>
2024-09-02 18:00:36 +0900

    240902

---
+++ boot01/src/test/java/com/yedam/app/Boot011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedam.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
